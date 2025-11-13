accedf75d4e1ea09747e2b38f803c0c397ca8dc0
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-13 20:51:55 +0500

    DOM

--- index.js
+++ index.js
@@ -33,3 +33,35 @@
 // for (const i = 0; i < 3; i++) {
 //   console.log(numberrr++);
 // }
+
+// const eltext = document.querySelector(".text");
+// eltext.innerHTML = "<b>Salom</b>";
+
+// console.log(eltext);
+
+const elH1 = document.createElement("h1");
+const elP = document.createElement("p");
+elH1.textContent = "men java scriptdan elon qilindim";
+const elBody = document.querySelector("body");
+
+elH1.setAttribute("class", "text");
+elH1.setAttribute("id", "title");
+
+elP.textContent = "men paragraphman";
+
+elBody.innerHTML = `
+
+
+   <div>
+      <img
+        src="https://thumbs.dreamstime.com/b/hiker-walking-along-scenic-mountain-ridge-path-sunrise-lone-wearing-yellow-jacket-follows-narrow-trail-stunning-381071486.jpg"
+        alt="bu yerda rasm bor"
+      />
+
+      <input type="text" placeholder="write some text" />
+      <button class="btn">Meni boss</button>
+
+      <button class="click">Click</button>
+    </div>
+
+`;
