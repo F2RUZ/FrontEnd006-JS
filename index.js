a0a6834053e1643d65ebb3229e99cc1224d1225b
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-27 20:46:44 +0500

    classlist

--- index.js
+++ index.js
@@ -1,57 +1,37 @@
-const elForm = document.querySelector(".form");
+// classList
+// add
+// remove
+//toggle
 
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const body = document.querySelector("body");
+const add = document.querySelector(".add"),
+  remove = document.querySelector(".remove"),
+  toggle = document.querySelector(".toggle"),
+  text = document.querySelector(".text");
 
-const passwordRegex =
-  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{}[\]:;"'<>.,?/~`]).{8,}$/;
+console.log(add, remove, toggle, text);
 
-// === Toast notification function ===
-const showToast = (text, color = "red") => {
-  Toastify({
-    text,
-    duration: 1500,
-    close: true,
-    gravity: "top",
-    position: "center",
-    stopOnFocus: true,
-    style: {
-      background:
-        color === "red"
-          ? "linear-gradient(to right, red, crimson)"
-          : "linear-gradient(to right, green, limegreen)",
-    },
-  }).showToast();
-};
 
-elForm.addEventListener("submit", (e) => {
-  e.preventDefault();
 
-  const username = elForm["username"].value.trim();
-  const passwd = elForm["passwd"].value.trim();
-  const email = elForm["email"].value.trim();
 
-  // === Validations ===
-  if (username.length < 3) {
-    return showToast("Username must be at least 3 chars");
-  }
 
-  if (!emailRegex.test(email)) {
-    return showToast("Invalid email format");
-  }
 
-  if (!passwordRegex.test(passwd)) {
-    return showToast(
-      "Password must contain letters, numbers, symbols and be 8+ chars"
-    );
-  }
+add.addEventListener("click", () => {
+  text.classList.add("add");
+});
 
-  // === If all valid ===
-  const user = {
-    username,
-    email,
-    passwd,
-  };
 
-  showToast("Successful validation", "green");
-  console.log(user);
+
+
+
+remove.addEventListener("click", () => {
+  text.classList.remove("add");
+});
+
+
+
+
+
+toggle.addEventListener("click", () => {
+  text.classList.toggle("add");
 });
