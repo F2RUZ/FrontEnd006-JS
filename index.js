b44c479f7f6442aaaa36dde662ae56bdc7217ee9
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-29 19:54:14 +0500

    events

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-// classList
-// add
-// remove
-//toggle
+//click
 
-const body = document.querySelector("body");
-const add = document.querySelector(".add"),
-  remove = document.querySelector(".remove"),
-  toggle = document.querySelector(".toggle"),
-  text = document.querySelector(".text");
+//dblclick
 
-console.log(add, remove, toggle, text);
+//submit
 
+// document.querySelector(".btn").addEventListener("", () => {
+//   alert("click types worked");
+// });
 
+// window.addEventListener("scroll", (e) => {
+//   console.log(e);
+// });
 
+// const input = document.querySelector(".name");
 
+// //form ,, input
 
+// input.addEventListener("focus", (e) => {
+//   console.log(e.target.value);
+// });
 
-add.addEventListener("click", () => {
-  text.classList.add("add");
+window.addEventListener("keydown", (e) => {
+  console.log(e);
 });
 
+let i = 0;
 
+while (i < 6) {
+  if (i === 3) {
+    break;
+  }
+  i = i + 1;
+}
 
+console.log(i);
+// Expected output: 3
 
+let text = "";
 
-remove.addEventListener("click", () => {
-  text.classList.remove("add");
-});
+for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  text = text + i;
+}
 
-
-
-
-
-toggle.addEventListener("click", () => {
-  text.classList.toggle("add");
-});
+console.log(text);
+// Expected output: "012456789"
