4c866a7f6949ad7c4d77a7aa83cf2e1f2560e896
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-30 13:11:27 +0500

    product

--- index.js
+++ index.js
@@ -1,49 +1,67 @@
-//click
-
-//dblclick
-
-//submit
-
-// document.querySelector(".btn").addEventListener("", () => {
-//   alert("click types worked");
-// });
-
-// window.addEventListener("scroll", (e) => {
-//   console.log(e);
-// });
-
-// const input = document.querySelector(".name");
-
-// //form ,, input
-
-// input.addEventListener("focus", (e) => {
-//   console.log(e.target.value);
-// });
-
-window.addEventListener("keydown", (e) => {
-  console.log(e);
+const request = new XMLHttpRequest();
+const elList = document.querySelector(".cards");
+const loading = document.querySelector(".loading");
+request.addEventListener("readystatechange", () => {
+  if (request.readyState === 4) {
+    const data = JSON.parse(request.responseText);
+    showData(data);
+  }
 });
 
-let i = 0;
+request.open("GET", "https://fakestoreapi.com/products");
 
-while (i < 6) {
-  if (i === 3) {
-    break;
-  }
-  i = i + 1;
+request.send();
+
+function showData(data) {
+  data.map(({ title, image, id, category, price, description }, index) => {
+    // console.log(item);
+
+    loading.innerHTML = "";
+    elList.innerHTML += `
+    
+      <article class="product-card" aria-label="Mens Cotton Jacket">
+        <div class="product-card__media">
+          <img
+          width="100%"
+          height= "200"
+            class="product-card__img"
+            src=${image}
+            alt="Mens Cotton Jacket"
+          />
+        </div>
+
+        <div class="product-card__body">
+          <div>
+            <h3 class="product-card__title">${title}</h3>
+            <div class="product-card__category">${category}</div>
+          </div>
+
+          <div class="product-card__price-row">
+            <div class="product-card__price">$${price}</div>
+            <div
+              class="product-card__rating"
+              title="4.7 out of 5 (500 reviews)"
+            >
+              <span class="star">★</span>
+              <span>4.7</span>
+              <small style="opacity: 0.6">(500)</small>
+            </div>
+          </div>
+
+          <p class="product-card__desc">
+       ${description}
+          </p>
+
+          <div class="product-card__actions">
+            <button class="btn btn--primary" type="button">Add to cart</button>
+            <button class="btn btn--ghost" type="button">View</button>
+          </div>
+        </div>
+      </article>
+    
+    
+    `;
+  });
 }
 
-console.log(i);
-// Expected output: 3
 
-let text = "";
-
-for (let i = 0; i < 10; i++) {
-  if (i === 3) {
-    continue;
-  }
-  text = text + i;
-}
-
-console.log(text);
-// Expected output: "012456789"
